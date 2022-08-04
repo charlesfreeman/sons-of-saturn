709d5ead31b4d6902697c9bf7f94be58cb41a0ae
Charles Freeman <cwilfreeman@gmail.com>
2022-08-04 19:18:47 -0400

    [Infirmary] Tracking all new assets and scenes (convos and pov's) in the Infirmary

---
+++ infirmary/tann_office/ClickToSearch.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+export var next_scene = ""
+
+onready var mag_glass = load("res://roaming_pov/images/mag_glass.png")
+
+
+func _ready():
+	pass
+
+
+func _on_ClickToSearch_mouse_entered():
+	Input.set_custom_mouse_cursor(mag_glass)
+
+
+func _on_ClickToSearch_mouse_exited():
+	Input.set_custom_mouse_cursor(null)
+
+
+func _on_ClickToSearch_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+		and event.button_index == BUTTON_LEFT \
+		and event.pressed:
+			SceneManager.change_scene(next_scene)

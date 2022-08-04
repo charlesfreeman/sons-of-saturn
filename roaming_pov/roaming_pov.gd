2b0432a4c6192b6836c134974682d581775838e1
Charles Freeman <cwilfreeman@gmail.com>
2022-08-04 19:15:18 -0400

    [RoamingPoV] added new disable and enable button features, map location setting, new node signal connections for popups.

--- roaming_pov/roaming_pov.gd
+++ roaming_pov/roaming_pov.gd
@@ -1,45 +1,65 @@
 extends HBoxContainer
 
 var global_scene_path = "None"
+var global_texture_path = "None"
 var pov_scene
 var pov_instance
 
+onready var camera = $VBoxContainer/TabContainer/Map/Viewport/MapRect/Camera2D
+onready var char_rect = $VBoxContainer/TabContainer/Map/Viewport/MapRect/CharRect
+onready var up_button = $VBoxContainer/HBoxContainer/GridContainer/UpButton
+onready var right_button = $VBoxContainer/HBoxContainer/GridContainer/RightButton
+onready var down_button = $VBoxContainer/HBoxContainer/GridContainer/DownButton
+onready var left_button = $VBoxContainer/HBoxContainer/GridContainer/LeftButton
+
 
 func _ready():
-	print("global location: ", Global.location)
-	pov_instance = load(Global.location).instance()
-	add_child(pov_instance)
-	move_child(pov_instance, 0)
-	_update_buttons()
+	global_scene_path = Global.location
+	_load_PoV_instance()
+	
+	Global.save_game()
 	
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_up"):
-		change_PoV($PoV.scene_up)
+		_on_UpButton_pressed()
 	elif Input.is_action_pressed("ui_right"):
-		change_PoV($PoV.scene_right)
+		_on_RightButton_pressed()
 	elif Input.is_action_pressed("ui_down"):
-		change_PoV($PoV.scene_down)
+		_on_DownButton_pressed()
 	elif Input.is_action_pressed("ui_left"):
-		change_PoV($PoV.scene_left)
+		_on_LeftButton_pressed()
 
-
-func _on_RightButton_pressed():
-	change_PoV(pov_instance.scene_right)
-
-
-func _on_DownButton_pressed():
-	change_PoV(pov_instance.scene_down)
-
-
-func _on_LeftButton_pressed():
-	change_PoV(pov_instance.scene_left)
 
 
 func _on_UpButton_pressed():
-	change_PoV(pov_instance.scene_up)
-	
-	
+		if pov_instance.non_roam_scene_up:
+			SceneManager.change_scene(pov_instance.scene_up)
+		else:
+			change_PoV(pov_instance.scene_up)
+
+
+func _on_RightButton_pressed():
+		if pov_instance.non_roam_scene_right:
+			SceneManager.change_scene(pov_instance.scene_right)
+		else:
+			change_PoV(pov_instance.scene_right)
+
+
+func _on_DownButton_pressed():
+		if pov_instance.non_roam_scene_down:
+			SceneManager.change_scene(pov_instance.scene_down)
+		else:
+			change_PoV(pov_instance.scene_down)
+
+
+func _on_LeftButton_pressed():
+		if pov_instance.non_roam_scene_left:
+			SceneManager.change_scene(pov_instance.scene_left)
+		else:
+			change_PoV(pov_instance.scene_left)
+
+
 func change_PoV(scene_path):
 	# load the next scene and unpack it into a node
 	if scene_path != "None":
@@ -47,30 +67,68 @@ func change_PoV(scene_path):
 		$TransitionScreen.transition()
 
 
-func _on_TransitionScreen_transitioned():
-	remove_child(pov_instance)
+func swap_texture():
+	$TransitionScreenTexture.transition()
+
+
+func _load_PoV_instance():
 	pov_scene = load(global_scene_path)
 	pov_instance = pov_scene.instance()
 	add_child(pov_instance)
 	move_child(pov_instance, 0)
 	_update_buttons()
-	
+	pov_instance.connect("move_pov_up", self, "_on_UpButton_pressed")
+	pov_instance.connect("move_pov_right", self, "_on_RightButton_pressed")
+	pov_instance.connect("move_pov_down", self, "_on_DownButton_pressed")
+	pov_instance.connect("move_pov_left", self, "_on_LeftButton_pressed")
+	pov_instance.connect("disable_buttons", self, "_disable_buttons")
+	pov_instance.connect("enable_buttons", self, "_enable_buttons")
+	pov_instance.connect("swap_bg", self, "swap_texture")
+
+
+func _on_TransitionScreen_transitioned():
+	remove_child(pov_instance)
+	_load_PoV_instance()
+
+
+func _on_TransitionScreenTexture_transitioned():
+	pov_instance.swap_bg()
+	get_tree().call_group("diff_bg", "make_visible")
+
 
 func _update_buttons():
-	if $PoV.scene_up == "None":
-		$VBoxContainer/HBoxContainer/GridContainer/UpButton.disabled = true
+	if pov_instance.scene_up == "None":
+		up_button.disabled = true
 	else:
-		$VBoxContainer/HBoxContainer/GridContainer/UpButton.disabled = false
-	if $PoV.scene_right == "None":
-		$VBoxContainer/HBoxContainer/GridContainer/RightButton.disabled = true
+		up_button.disabled = false
+	if pov_instance.scene_right == "None":
+		right_button.disabled = true
 	else:
-		$VBoxContainer/HBoxContainer/GridContainer/RightButton.disabled = false
-	if $PoV.scene_down == "None":
-		$VBoxContainer/HBoxContainer/GridContainer/DownButton.disabled = true
+		right_button.disabled = false
+	if pov_instance.scene_down == "None":
+		down_button.disabled = true
 	else:
-		$VBoxContainer/HBoxContainer/GridContainer/DownButton.disabled = false
-	if $PoV.scene_left == "None":
-		$VBoxContainer/HBoxContainer/GridContainer/LeftButton.disabled = true
+		down_button.disabled = false
+	if pov_instance.scene_left == "None":
+		left_button.disabled = true
 	else:
-		$VBoxContainer/HBoxContainer/GridContainer/LeftButton.disabled = false
+		left_button.disabled = false
 	
+	
+func _disable_buttons():
+	up_button.disabled = true
+	right_button.disabled = true
+	down_button.disabled = true
+	left_button.disabled = true
+	
+
+func _enable_buttons():
+	_update_buttons()
+	
+	
+func set_pos(pos):
+	char_rect.rect_position.x = pos.x
+	char_rect.rect_position.y = pos.y
+	camera.position.x = pos.x
+	camera.position.y = pos.y
+

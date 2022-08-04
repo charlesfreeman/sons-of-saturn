8b9239bf1a63fcea3eaa3df147417d7f7e3177f0
Charles Freeman <cwilfreeman@gmail.com>
2022-08-04 18:57:12 -0400

    [Global] added simple saving capability and progression flags

--- Global.gd
+++ Global.gd
@@ -1,11 +1,25 @@
 extends Node
 
-var location = "res://sewer/just_stood_up/just_stood_up.tscn"
-var party = ["Rando"]
+var location = "res://infirmary/overgrowth_pink_hallway/overgrowth_pink_hallway.tscn"
+var party = ["Amelie" ,"Wiggly"]
+var active_popup = false
+var prog_flags = {
+	"exited_morgue" : false,
+	"vera_office_convo" : false,
+	"tann_office_convo" : false,
+	"trash_chute_convo" : false,
+	"cell_grate_convo" : false,
+}
 
 func _ready():
 	pass
+
+func flip_prog_flag(flag: String):
+	prog_flags[flag] = true
 	
+func get_prog_flag(flag: String):
+	return prog_flags[flag]
+
 func add_to_party(char_name: String):
 	if not char_name in party:
 		party.append(char_name)
@@ -18,3 +32,27 @@ func remove_from_party(char_name: String):
 	else:
 		print("Warning: cannot remove %s from party", char_name)
 
+func produce_save_dict():
+	var save_dict = {
+		"location" : location,
+		"party" : party
+	}
+	return save_dict
+	
+func save_game():
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	var save_data = produce_save_dict()
+	save_game.store_line(to_json(save_data))
+	save_game.close()
+	
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return # Error! We don't have a save to load.
+	save_game.open("user://savegame.save", File.READ)
+	var save_data = parse_json(save_game.get_line())
+	print(save_data.keys())
+	location = save_data["location"]
+	party = save_data["party"]
+	SceneManager.change_scene("res://roaming_pov/roaming_pov.tscn")

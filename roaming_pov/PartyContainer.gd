ecb8b3ebb2ddbda968f3a531b00c47b8d1513cbb
Charles Freeman <cwilfreeman@gmail.com>
2022-10-11 10:57:44 -0400

    [PartyContainer] script modified to only add Wiggly, Amelie assumed present

--- roaming_pov/PartyContainer.gd
+++ roaming_pov/PartyContainer.gd
@@ -1,13 +1,16 @@
 extends HBoxContainer
 
 var character_dict = {
-	"Amelie" : "res://images/amelie_270x360_no_mirror.png",
-	"Wiggly" : "res://images/wigley_looking_left_230x307.png",
+	"Wiggly" : "res://conversation_pov/char_profiles/wiggly/wiggly_sad.png",
 }
 
 
 func _ready():
+	var char_texture_scene = load("res://roaming_pov/PartyMember.tscn")
 	for character in Global.party:
-		var char_texture = TextureRect.new()
-		char_texture.texture = load(character_dict[character])
-		add_child(char_texture)
+		if character != "":
+			print("Character: ", character)
+			var char_texture = char_texture_scene.instance()
+			char_texture.rect_min_size.x = 359
+			char_texture.texture = load(character_dict[character])
+			add_child(char_texture)

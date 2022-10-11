ccb1a985b52a160619053b4ba1e4c60facbe9802
Charles Freeman <cwilfreeman@gmail.com>
2022-10-11 10:50:31 -0400

    [Convo] face resources and corresponding infrastructure

--- conversation_pov/CharacterRect.gd
+++ conversation_pov/CharacterRect.gd
@@ -1,5 +1,6 @@
 extends TextureRect
 
+export var on_start = false
 export var appear_tag = ""
 export var disappear_tag = ""
 
@@ -9,13 +10,14 @@ func _ready():
 	  Color(1, 1, 1, 0), Color(1, 1, 1, 1), 0.3, 
 	  Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$TweenOut.interpolate_property(self, "modulate", 
-	  Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.3, 
+	  Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.2, 
 	  Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if on_start:
+		$TweenIn.start()
 
 
-func _on_HBoxContainer_tag(tag):
+func appear_disappear(tag):
 	if tag == appear_tag:
 		$TweenIn.start()
 	elif tag == disappear_tag:
 		$TweenOut.start()
-

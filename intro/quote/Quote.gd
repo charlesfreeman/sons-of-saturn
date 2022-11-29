d9a2d5c1bb00210b6d96be253e687824ae31c251
Charles Freeman <cwilfreeman@gmail.com>
2022-11-29 15:14:35 -0500

    [Audio] added audio to title screen and intro

--- intro/quote/Quote.gd
+++ intro/quote/Quote.gd
@@ -2,6 +2,7 @@ extends TextureRect
 
 
 func _ready():
+	Audio.get_node("AudioStreamPlayer").play()
 	yield(get_tree().create_timer(7.0), "timeout")
 	var options = SceneManager.create_options()
 	var general_options = SceneManager.create_general_options()

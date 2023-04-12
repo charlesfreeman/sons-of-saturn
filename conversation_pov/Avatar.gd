4af7577a66fb96bc3d3ba5e93d865de0ebd83687
Charles Freeman <cwilfreeman@gmail.com>
2023-04-11 22:00:32 -0400

    [Avatar] removed hard coding

--- conversation_pov/Avatar.gd
+++ conversation_pov/Avatar.gd
@@ -10,15 +10,17 @@ var d_level = 0.75
 func _ready():
 	pass
 
+
 func darken():
 	tween_darken.interpolate_property(self, "modulate", 
-	  Color(1, 1, 1, 1), Color(d_level, d_level, d_level, o_level), 0.05,
+	  self.modulate, Color(d_level, d_level, d_level, o_level), 0.1,
 	  Tween.TRANS_QUART, Tween.EASE_OUT)
 	tween_darken.start()
 	
 
 func brighten():
 	tween_brighten.interpolate_property(self, "modulate", 
-	  Color(d_level, d_level, d_level, o_level), Color(1, 1, 1, 1), 0.05, 
+	  self.modulate, Color(1, 1, 1, 1), 0.1, 
 	  Tween.TRANS_QUART, Tween.EASE_OUT)
 	tween_brighten.start()
+

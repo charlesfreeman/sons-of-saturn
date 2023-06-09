effd71d66d1a1cde3083152e3e3efb83a7b87c17
Charles Freeman <cwilfreeman@gmail.com>
2023-06-08 21:27:32 -0400

    [Transition] removed vestigial lines

--- transition/TransitionScreen.gd
+++ transition/TransitionScreen.gd
@@ -10,13 +10,9 @@ func _ready():
 
 func transition():
 	$AnimationPlayer.play("fade_to_black")
-	print("Fading to black")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
-		print("emit signal transitioned")
 		emit_signal("transitioned")
 		$AnimationPlayer.play("fade_to_normal")
-	if anim_name == "fade_to_normal":
-		print("finished fading")

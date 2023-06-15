2828b974b9e794d586e853ea891589962e094d04
Charles Freeman <cwilfreeman@gmail.com>
2023-06-15 15:20:52 -0400

    [TitleMenuOpt] no longer showing quote on new game

--- title_screen/TitleMenuOpt.gd
+++ title_screen/TitleMenuOpt.gd
@@ -36,7 +36,9 @@ func _on_Continue_pressed():
 
 
 func _on_NewGame_pressed():
+	Global.change_song("None")
+	Global.change_soundscape("FreshAir")
 	var options = SceneManager.create_options()
 	var general_options = SceneManager.create_general_options()
-	SceneManager.change_scene("Quote", options, options, general_options)
+	SceneManager.change_scene("Stage", options, options, general_options)
 

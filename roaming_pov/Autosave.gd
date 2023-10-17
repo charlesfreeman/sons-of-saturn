bd70942e02dbbf850b8ff485cdf62728f7dc2c0c
Charles Freeman <cwilfreeman@gmail.com>
2023-10-17 14:29:25 -0400

    [Godot4] changed tweens in autosave to be G4 compatible

--- roaming_pov/Autosave.gd
+++ roaming_pov/Autosave.gd
@@ -1,22 +1,21 @@
 extends HBoxContainer
 
-@onready var fadein = $TweenFadeIn
-@onready var hold = $TweenHold
-@onready var fadeout = $TweenFadeOut
+@onready var tween_fadein = create_tween()
+@onready var tween_hold = create_tween()
+@onready var tween_fadeout = create_tween()
 
 
 func _ready():
 	pass
 
 func save():
-	fadein.interpolate_property(self, "modulate", self.modulate, Color(1, 1, 1, 1), 0.15, Tween.TRANS_QUAD, Tween.EASE_IN)
-	fadein.start()
+	tween_fadein.tween_property(self, "modulate", Color(1, 1, 1, 1), 
+	  0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 
 func _on_TweenFadeIn_tween_completed(_object, _key):
-	hold.interpolate_property(self, "modulate", self.modulate, self.modulate, 0.75, Tween.TRANS_QUAD, Tween.EASE_IN)
-	hold.start()
+	tween_hold.tween_property(self, "modulate", self.modulate, 0.75)
 
 
 func _on_TweenHold_tween_completed(_object, _key):
-	fadeout.interpolate_property(self, "modulate", self.modulate, Color(0, 0, 0, 0), 0.15, Tween.TRANS_QUAD, Tween.EASE_IN)
-	fadeout.start()
+	tween_fadeout.tween_property(self, "modulate", Color(0, 0, 0, 0), 
+	  0.15).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)

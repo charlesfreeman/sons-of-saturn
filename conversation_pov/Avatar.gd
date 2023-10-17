d66f838097f2e0abd3fb53614d621d0b737e1c4c
Charles Freeman <cwilfreeman@gmail.com>
2023-10-17 14:28:59 -0400

    [Godot4] changed tweens in conversation_pov to be G4 compatible

--- conversation_pov/Avatar.gd
+++ conversation_pov/Avatar.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
-@onready var tween_darken = $TweenDarken
-@onready var tween_brighten = $TweenBrighten
+@onready var tween_darken = create_tween() 
+@onready var tween_brighten =  create_tween()
 
 var o_level = 0.75
 var d_level = 0.75
@@ -12,15 +12,11 @@ func _ready():
 
 
 func darken():
-	tween_darken.interpolate_property(self, "modulate", 
-	  self.modulate, Color(d_level, d_level, d_level, o_level), 0.1,
-	  Tween.TRANS_QUART, Tween.EASE_OUT)
-	tween_darken.start()
+	tween_darken.tween_property(self, "modulate", Color(d_level, d_level, d_level, o_level), 
+	  0.1).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 	
 
 func brighten():
-	tween_brighten.interpolate_property(self, "modulate", 
-	  self.modulate, Color(1, 1, 1, 1), 0.1, 
-	  Tween.TRANS_QUART, Tween.EASE_OUT)
-	tween_brighten.start()
+	tween_brighten.tween_property(self, "modulate", Color(1, 1, 1, 1), 
+	  0.1).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 
--- conversation_pov/CharacterRect.gd
+++ conversation_pov/CharacterRect.gd
@@ -1,5 +1,12 @@
 extends TextureRect
 
+@onready var tween_in = create_tween()
+@onready var tween_out = create_tween()
+@onready var tween_out_slow = create_tween()
+@onready var tween_brighten = create_tween()
+@onready var tween_darken = create_tween()
+@onready var tween_appear_dark = create_tween()
+
 @export var on_start = false
 @export var dark_on_start = false
 @export var appear_tag = ""
@@ -11,48 +18,31 @@ extends TextureRect
 
 
 func _ready():
-	# tween from going transparent to fully visible
-	$TweenIn.interpolate_property(self, "modulate", 
-	  self.modulate, Color(1, 1, 1, 1), 0.3, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# tween for going visible to fully transparent
-	$TweenOut.interpolate_property(self, "modulate", 
-	  self.modulate, Color(1, 1, 1, 0), 0.2, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# same thing but slower
-	$TweenOutSlow.interpolate_property(self, "modulate", 
-	  self.modulate, Color(1, 1, 1, 0), 1, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# tween for going fully visible
-	$TweenBrighten.interpolate_property(self, "modulate", 
-	  self.modulate, Color(1, 1, 1, 1), 0.3, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# tween for going fully visible to mostly dark
-	$TweenDarken.interpolate_property(self, "modulate", 
-	  self.modulate, Color(0.1, 0.1, 0.1, 1), 0.2, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# tween for going transparent to mostly dark
-	$TweenAppearDark.interpolate_property(self, "modulate", 
-	  Color(0, 0, 0, 0), Color(0.35, 0.35, 0.35, 1), 0.8, 
-	  Tween.TRANS_LINEAR, Tween.EASE_IN)
-	# might want to add dark -> tranparent tween at some point but not yet needed
 	if on_start:
-		$TweenIn.start()
+		tween_in.tween_property(self, "modulate", Color(1, 1, 1, 1), 
+		  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 	elif dark_on_start:
-		$TweenAppearDark.start()
+		tween_darken.tween_property(self, "modulate", Color(0.35, 0.35, 0.35, 1), 
+		  0.8).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 
 
 func appear_disappear(tag):
 	match tag:
 		appear_tag:
-			$TweenIn.start()
+			tween_in.tween_property(self, "modulate", Color(1, 1, 1, 1), 
+			  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		disappear_tag:
-			$TweenOut.start()
+			tween_out.tween_property(self, "modulate", Color(1, 1, 1, 0), 
+			  0.2).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		disappear_slow_tag:
-			$TweenOutSlow.start()
+			tween_out_slow.tween_property(self, "modulate", Color(1, 1, 1, 0), 
+			  1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		brighten_tag:
-			$TweenBrighten.start()
+			tween_brighten.tween_property(self, "modulate", Color(1, 1, 1, 1), 
+			  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		darken_tag:
-			$TweenDarken.start()
+			tween_darken.tween_property(self, "modulate", Color(0.1, 0.1, 0.1, 1), 
+			  0.2).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		appear_dark_tag:
-			$TweenAppearDark.start()
+			tween_darken.tween_property(self, "modulate", Color(0.35, 0.35, 0.35, 1), 
+			  0.8).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)

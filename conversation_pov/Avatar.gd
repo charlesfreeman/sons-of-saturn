42903187db49664f4d163eb376e869eaf55ebec9
Charles Freeman <cwilfreeman@gmail.com>
2023-10-18 10:21:09 -0400

    [Tweens] fixed tweens in conversation_pov

--- conversation_pov/Avatar.gd
+++ conversation_pov/Avatar.gd
@@ -1,8 +1,5 @@
 extends TextureRect
 
-@onready var tween_darken = create_tween() 
-@onready var tween_brighten =  create_tween()
-
 var o_level = 0.75
 var d_level = 0.75
 
@@ -12,11 +9,13 @@ func _ready():
 
 
 func darken():
+	var tween_darken = create_tween() 
 	tween_darken.tween_property(self, "modulate", Color(d_level, d_level, d_level, o_level), 
 	  0.1).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 	
 
 func brighten():
+	var tween_brighten =  create_tween()
 	tween_brighten.tween_property(self, "modulate", Color(1, 1, 1, 1), 
 	  0.1).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 
--- conversation_pov/CharacterRect.gd
+++ conversation_pov/CharacterRect.gd
@@ -1,12 +1,5 @@
 extends TextureRect
 
-@onready var tween_in = create_tween()
-@onready var tween_out = create_tween()
-@onready var tween_out_slow = create_tween()
-@onready var tween_brighten = create_tween()
-@onready var tween_darken = create_tween()
-@onready var tween_appear_dark = create_tween()
-
 @export var on_start = false
 @export var dark_on_start = false
 @export var appear_tag = ""
@@ -19,9 +12,11 @@ extends TextureRect
 
 func _ready():
 	if on_start:
+		var tween_in = create_tween()
 		tween_in.tween_property(self, "modulate", Color(1, 1, 1, 1), 
 		  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 	elif dark_on_start:
+		var tween_darken = create_tween()
 		tween_darken.tween_property(self, "modulate", Color(0.35, 0.35, 0.35, 1), 
 		  0.8).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 
@@ -29,20 +24,26 @@ func _ready():
 func appear_disappear(tag):
 	match tag:
 		appear_tag:
+			var tween_in = create_tween()
 			tween_in.tween_property(self, "modulate", Color(1, 1, 1, 1), 
 			  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		disappear_tag:
+			var tween_out = create_tween()
 			tween_out.tween_property(self, "modulate", Color(1, 1, 1, 0), 
 			  0.2).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		disappear_slow_tag:
+			var tween_out_slow = create_tween()
 			tween_out_slow.tween_property(self, "modulate", Color(1, 1, 1, 0), 
 			  1).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		brighten_tag:
+			var tween_brighten = create_tween()
 			tween_brighten.tween_property(self, "modulate", Color(1, 1, 1, 1), 
 			  0.3).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		darken_tag:
+			var tween_darken = create_tween()
 			tween_darken.tween_property(self, "modulate", Color(0.1, 0.1, 0.1, 1), 
 			  0.2).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 		appear_dark_tag:
-			tween_darken.tween_property(self, "modulate", Color(0.35, 0.35, 0.35, 1), 
+			var tween_appear_dark = create_tween()
+			tween_appear_dark.tween_property(self, "modulate", Color(0.35, 0.35, 0.35, 1), 
 			  0.8).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)

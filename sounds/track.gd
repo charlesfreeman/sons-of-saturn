2fde6c7ada247cceb160622bbb1b91504badc574
Charles Freeman <cwilfreeman@gmail.com>
2023-10-18 15:49:34 -0400

    [track] G4 compat changes (tween)

--- sounds/track.gd
+++ sounds/track.gd
@@ -2,23 +2,24 @@ extends AudioStreamPlayer
 
 var old_db
 
-@onready var tween = create_tween()
-
 
 func _ready():
-	tween.connect("tween_completed", Callable(self, "reset_old_song"))
+	pass
 
 
 func fade_out():
 	old_db = self.volume_db
-	tween.tween_property(self, "volume_db", -60, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tween.connect("finished", Callable(self, "reset_old_song"))
+	tween.tween_property(self, "volume_db", -60, 1)
 	
 	
 func fade_in():
 	old_db = self.volume_db
 	self.volume_db = -60
 	self.playing = true
-	tween.tween_property(self, "volume_db", old_db, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "volume_db", old_db, 0.1)
 	
 
 func reset_old_song(_object, _key):

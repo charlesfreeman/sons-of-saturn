bbb1a3279c24aee4d89b6aca63377e5650be4cc7
Charles Freeman <cwilfreeman@gmail.com>
2023-10-18 10:20:11 -0400

    [Tweens] fixed tweens in inspection_room_convo

--- maternity/inspection_room_convo/RedBG.gd
+++ maternity/inspection_room_convo/RedBG.gd
@@ -1,7 +1,5 @@
 extends TextureRect
 
-@onready var tween = create_tween()
-
 
 func _ready():
 	pass
@@ -10,19 +8,19 @@ func _ready():
 func _on_Convo_tag(tag):
 	if tag == "red_bg_appear":
 		# tween for going fully visible to totally dark and transparent
-		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 
-		  0.5).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.5)
 	elif tag == "red_bg_darken":
-		tween.tween_property(self, "modulate", Color(1, 0, 0, 1), 
-		  0.25).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "modulate", Color(1, 0, 0, 1), 0.25)
 
 
 func _on_TweenDarken_tween_completed(object, key):
-	tween.tween_property(self, "modulate", Color(1, 0.5, 0.5, 1), 
-	  5).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "modulate", Color(1, 0.5, 0.5, 1), 5)
 
 
 func _on_TweenPartial_tween_completed(object, key):
-	tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 
-	  0.2).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 0.2)
 
--- maternity/inspection_room_convo/ThornBG.gd
+++ maternity/inspection_room_convo/ThornBG.gd
@@ -1,8 +1,5 @@
 extends TextureRect
 
-@onready var tween_appear = create_tween()
-@onready var tween_disappear = create_tween()
-
 
 func _ready():
 	pass
@@ -13,11 +10,11 @@ func _on_Convo_tag(tag):
 		# play sound effect
 		$SwitchFlipSlow.play()
 		# tween for going fully visible to totally dark and transparent
-		tween_appear.tween_property(self, "modulate", Color(1, 1, 1, 1), 
-		  0.75).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.75)
 
 
 func _on_TweenAppear_tween_completed(object, key):
 	# tween for going fully visible to totally dark and transparent
-	tween_disappear.tween_property(self, "modulate", Color(0, 0, 0, 0), 
-	  0.5).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "modulate", Color(0, 0, 0, 0), 0.5)

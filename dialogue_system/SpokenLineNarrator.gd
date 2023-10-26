3fe41b722f0ab03063dbad15b59c7cd7318a7f2d
Charles Freeman <cwilfreeman@gmail.com>
2023-10-26 19:12:41 -0400

    [SpokenLineNarrator] added lines to resize font for text

--- dialogue_system/SpokenLineNarrator.gd
+++ dialogue_system/SpokenLineNarrator.gd
@@ -2,8 +2,12 @@ extends Label
 
 
 func _ready():
-	pass
+	set_font_size()
 
 
 func make_grey():
 	self.modulate = Color(Global.dbrightness, Global.dbrightness, Global.dbrightness, 1)
+
+
+func set_font_size():
+	self.add_theme_font_size_override("font_size", Global.font_size)

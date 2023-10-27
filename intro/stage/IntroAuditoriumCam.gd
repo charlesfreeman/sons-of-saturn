b588e98d39e00f2ad82a19fa6e6ff5a18eeacb64
Charles Freeman <cwilfreeman@gmail.com>
2023-10-27 12:31:04 -0400

    [IntroCam] refactored to support new await-finished schema

--- intro/stage/IntroAuditoriumCam.gd
+++ intro/stage/IntroAuditoriumCam.gd
@@ -5,10 +5,11 @@ extends Camera2D
 
 func _ready():
 	var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "zoom", Vector2(1.33, 1.33), 30)
+	await tween.tween_property(self, "zoom", Vector2(1.33, 1.33), 30).finished
+	self._on_tween_completed()
 
 
-func _on_Tween_tween_completed(object, key):
+func _on_tween_completed():
 	await get_tree().create_timer(2.0).timeout
 	var options = SceneManager.create_options()
 	var general_options = SceneManager.create_general_options()

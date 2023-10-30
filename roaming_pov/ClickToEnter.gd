fbbd351ab3de1feb3cb425fecb89c5e4c250f212
Charles Freeman <cwilfreeman@gmail.com>
2023-10-30 18:39:25 -0400

    [ClickToEnter] refactored to make use of G4 features (input pickability)

--- roaming_pov/ClickToEnter.gd
+++ roaming_pov/ClickToEnter.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
+var in_area = false
+
 @export var up_path = false
 @export var right_path = false
 @export var down_path = false
 @export var left_path = false
-
-var enabled = true
 
 signal up
 signal right
@@ -23,40 +23,49 @@ func _ready():
 
 
 func _on_ClickToEnter_mouse_entered():
-	if self.enabled:
-		if up_path:
-			Global.set_cursor("pointer_up")
-		elif right_path:
-			Global.set_cursor("pointer_right")
-		elif down_path:
-			Global.set_cursor("pointer_down")
-		elif left_path:
-			Global.set_cursor("pointer_left")
+	in_area = true
+	if up_path:
+		Global.set_cursor("pointer_up")
+	elif right_path:
+		Global.set_cursor("pointer_right")
+	elif down_path:
+		Global.set_cursor("pointer_down")
+	elif left_path:
+		Global.set_cursor("pointer_left")
 		
 
 func _on_ClickToEnter_mouse_exited():
-	if self.enabled:
-		Global.set_cursor("null")
+	in_area = false
+	Global.enter_release_cursor()
+
+
+func _signal_path():
+	if up_path:
+		emit_signal("up")
+	elif right_path:
+		emit_signal("right")
+	elif down_path:
+		emit_signal("down")
+	elif left_path:
+		emit_signal("left")
 
 
 func _on_ClickToEnter_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton \
+	if (event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
-	and event.pressed:
-		if self.enabled:
-			if up_path:
-				emit_signal("up")
-			elif right_path:
-				emit_signal("right")
-			elif down_path:
-				emit_signal("down")
-			elif left_path:
-				emit_signal("left")
+	and event.pressed):
+		self._signal_path()
 
+
+func _input(event):
+	# using input_pickable as proxy for enabled / disabled
+	if Input.is_action_pressed("ui_accept") and in_area and self.input_pickable:
+		self._signal_path()
+		
 
 func enable():
-	self.enabled = true
+	self.input_pickable = true
 		
 		
 func disable():
-	self.enabled = false
+	self.input_pickable = false

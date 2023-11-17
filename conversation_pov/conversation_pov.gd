cf04a55c45817651803b8237402d6c7ce7c3867b
Charles Freeman <cwilfreeman@gmail.com>
2023-11-17 10:33:49 -0500

    [Frost] added frost char prof

--- conversation_pov/conversation_pov.gd
+++ conversation_pov/conversation_pov.gd
@@ -104,6 +104,7 @@ var misc_profiles = {
 	"Vera" : "res://conversation_pov/char_profiles/misc/vera_profile.png",
 	"Leslie" : "res://conversation_pov/char_profiles/misc/leslie_in_mirror.png",
 	"Robert" : "res://conversation_pov/char_profiles/misc/robert_in_mirror.png",
+	"Frost" : "res://conversation_pov/char_profiles/misc/frost_profile.png",
 }
 
 # need to track state of subjects for changing profiles mid convo
